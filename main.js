5df01d0de36b64e97c3726f099bfb51b15ad331a
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-04-09 13:49:17 +0300

    this input listener counts the words

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const paragraph = document.getElementById("paragraph");
+const wordDiv = document.getElementById("word");
 const btn = document.querySelector("button");
 
 paragraph.addEventListener("input", function (e) {
   paragraph.textContent = e.target.value;
-  const words = paragraph.textContent.split(" ");
-  console.log(words.length);
-
+  const words = paragraph.textContent.trim().split(" ");
+  if (paragraph.textContent === "") {
+    wordDiv.textContent = 0;
+  } else {
+    wordDiv.textContent = words.length;
+  }
   console.log(paragraph);
+  console.log(typeof paragraph.textContent);
 });
-
-// const words = paragraph.split(" ");
