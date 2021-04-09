22dd9b31857125b5d457bf5fe12b5720108f4611
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-04-09 15:54:02 +0300

    finished speech function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const paragraph = document.getElementById("paragraph");
 const wordDiv = document.getElementById("word");
 const btn = document.querySelector("button");
+const microphone = document.querySelector("#microphone");
 
 paragraph.addEventListener("input", function (e) {
   paragraph.textContent = e.target.value;
@@ -12,4 +13,16 @@ paragraph.addEventListener("input", function (e) {
   }
   console.log(paragraph);
   console.log(typeof paragraph.textContent);
+});
+
+microphone.addEventListener("click", function () {
+  const speech = new SpeechSynthesisUtterance();
+  speech.lang = "en-US";
+
+  speech.text = paragraph.textContent;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
 });
